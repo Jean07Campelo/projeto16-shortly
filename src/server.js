3d569d2e1a854a68720ee02b072d406a6a0753a7
Jean Campelo <jean93campelo@gmail.com>
2022-10-12 09:56:37 -0300

    Prepare for deploy

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const server = express();
 server.use(cors());
 server.use(express.json());
-const PORT = 4000;
+
+const PORT = process.env.PORT
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
