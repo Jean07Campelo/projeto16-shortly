e6f02fc5089a1f23c8b664a9f718b64a8ba4bda6
Jean Campelo <jean93campelo@gmail.com>
2022-10-17 15:50:20 -0300

    Retornando arquivo dump.sql na estrutura original

---
+++ dump.sql
@@ -0,0 +1,23 @@
+CREATE TABLE users (
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(50) NOT NULL,
+   email VARCHAR(50) UNIQUE NOT NULL,
+   "passwordHash" TEXT UNIQUE NOT NULL,
+   "createdAt" TEXT NOT NULL
+);
+ 
+CREATE TABLE sessions (
+   id SERIAL PRIMARY KEY,
+   "userId" INTEGER REFERENCES "users"("id"),
+   token VARCHAR(100) NOT NULL,
+   "createdAt" VARCHAR(20) NOT NULL
+);
+ 
+CREATE TABLE urls (
+   id SERIAL PRIMARY KEY,
+   "userId" INTEGER REFERENCES "users"("id"),
+   url TEXT UNIQUE NOT NULL,
+   "shortUrl" TEXT UNIQUE NOT NULL,
+   visits INTEGER DEFAULT 0,
+   "createdAt" VARCHAR(20) NOT NULL
+);
